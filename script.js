1486af3f3ec359bfb395728e90bf191c0ed3556d
git-user-ui <pratikpp2302@gmail.com>
2025-11-30 12:20:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const counterAdd = document.getElementById("counter");
+const btn = document.getElementById("incrementBtn");
+
+function increaseCounter() {
+  alert(counterAdd.textContent);
+  let currentValue = parseInt(counterAdd.textContent, 10)||0; 
+  counterAdd.textContent = currentValue + 1;
+}
+
+btn.addEventListener("click", increaseCounter);
